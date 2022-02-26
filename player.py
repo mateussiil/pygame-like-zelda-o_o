b1610698fdd165c2ab95ee53b4f12d74fedf62eb
Mateus Silva <mateus_silva97@hotmail.com>
2022-02-26 16:54:35 -0300

    feat(class player)

---
+++ player.py
@@ -0,0 +1,34 @@
+import pygame
+from typing import Literal
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, obstacle_sprites):
+        super().__init__(groups)
+        self.image = pygame.image.load('./graphic/test/player.png').convert_alpha()
+        self.rect: pygame.rect.Rect = self.image.get_rect(topleft = pos)
+
+        self.direction = pygame.math.Vector2()
+        self.speed: int = 5
+
+        self.obstacle_sprites = obstacle_sprites
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+    
+    def update(self):
+        self.input()
