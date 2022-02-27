5754b044d55643429d69d339ef7cfdb56f83139e
Mateus Silva <mateus_silva97@hotmail.com>
2022-02-27 00:35:09 -0300

    refactor(map): add other graphic and layout in map

--- level.py
+++ level.py
@@ -1,6 +1,8 @@
+from random import choice
 import pygame
 import pygame.freetype  # Import the freetype module.
-from settings import * 
+from settings import *
+from support import import_csv_layout, import_folder 
 from tile import Tile
 from player import Player
 from debug import debug
@@ -19,14 +21,32 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    self.player: Player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
+        layouts = {
+            'boundary': import_csv_layout('./map/map_FloorBlocks.csv'),
+            'grass': import_csv_layout('./map/map_Grass.csv'),
+            'object': import_csv_layout('./map/map_LargeObjects.csv')
+        }
+
+        graphics = {
+            'grass': import_folder('./graphics/Grass'),
+            'objects': import_folder('./graphics/objects'),
+        }
+
+        for style, layout in layouts.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x,y), [self.obstacle_sprites], 'invisible')
+                        if style == 'grass':
+                            random_grass_image = choice(graphics['grass'])
+                            Tile((x,y), [self.obstacle_sprites, self.visible_sprites], 'grass', random_grass_image)
+                        if style == 'object':
+                            surf = graphics['objects'][int(col)]
+                            Tile((x,y), [self.obstacle_sprites, self.visible_sprites], 'object', surf)
+        self.player: Player = Player((2000,1430), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         #update and draw the game
